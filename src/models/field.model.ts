4c2bcfe077652ad0f93950fe2fbd7ac3cda08cfb
Joost Zöllner <joostzoellner@online.de>
2018-02-13 13:31:05 +0100

    chore: Removed unused class

--- src/models/field.model.ts
+++ src/models/field.model.ts
@@ -2,10 +2,3 @@ export interface Field {
     amount: number;
     max: number;
 }
-
-export class Field implements Field {
-    constructor(max: number) {
-        this.amount = 0;
-        this.max = max;
-    }
-}
