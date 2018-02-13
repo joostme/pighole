719725188663e2685769365b38625e0f06039955
Joost Zöllner <joostzoellner@online.de>
2018-02-13 14:36:32 +0100

    1.0.1

--- dist/src/lib/util.js
+++ dist/src/lib/util.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 function rollDice() {
-    return Math.floor(Math.random() * 5 + 1);
+    return Math.floor(Math.random() * 6 + 1);
 }
 exports.rollDice = rollDice;
